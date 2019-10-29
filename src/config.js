ff2210548e65676ecb21152e7d26dd4b5e310d71
Dailer <Dailer@fanruan.com>
2019-10-29 17:37:33 +0800

    update

--- src/config.js
+++ src/config.js
@@ -40,7 +40,7 @@ exports.configs = {
                     "title": "又到了记日志的时候啦",
                     "description": "任务千万个 日志第一位 日志不规范 工资两行泪",
                     "url": "about:blank",
-                    "picurl": "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1572000832962&di=449fa2809c4db41a89e467c2b2027cc1&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201805%2F25%2F20180525233420_2aCmc.jpeg"
+                    "picurl": "http://imgsrc.baidu.com/forum/pic/item/d058ccbf6c81800a7327fdaab13533fa838b4757.jpg?v=tbs"
                 }
             ]
         }
