4f324b9ecedfd6cd06313ad89e3b739ced0620b2
zsmj1994 <zsmj1994@gmail.com>
2019-11-15 16:10:29 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,43 +1,31 @@
 const request = require('request')
 const schedule = require('node-schedule');
+const configs = require('../config.json')
 
 const sendMsg = (configs) => {
-    let cfg
-    configs && (cfg = configs)
-    console.log("current time", (new Date()).toLocaleTimeString())
-    console.log("webhook:", cfg.webhook)
 
     request({
         json: true,
         method: 'POST',
-        url: cfg.webhook,
-        body: cfg.msg
+        url: configs.webhook,
+        body: configs.msg
     }, (err, res, body) => {
-
-    })
-}
-
-const requestConfig = () => {
-    return new Promise((resolve, reject) => {
-        request('https://raw.githubusercontent.com/zsmj1994/decision-worklog-remind/master/config.json', (error, response, body) => {
-            if (error) {
-                reject(error);
-            } else {
-                resolve(JSON.parse(body));
-            }
-        })
+        if (err) {
+            console.error(err)
+        } else {
+            console.log("message sent", (new Date()).toLocaleTimeString())
+            console.log("message content", JSON.stringify(configs.msg))
+        }
     })
 }
 
 
 const scheduleObjectLiteralSyntax = (configs) => {
-    console.log("start")
+    console.log("job start, current time", (new Date()).toLocaleTimeString())
     configs.timers.forEach(timer => {
-        schedule.scheduleJob(timer, () => requestConfig().then(configs => sendMsg(configs)), error => sendMsg())
+        schedule.scheduleJob(timer, () => sendMsg(configs))
     });
 }
 
 
-requestConfig().then(configs => {
-    scheduleObjectLiteralSyntax(configs)
-})
+scheduleObjectLiteralSyntax(configs)
