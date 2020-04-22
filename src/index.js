696d222647380d616b82cea839421e49965049b5
zsmj1994 <zsmj1994@gmail.com>
2020-04-22 18:25:06 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,39 +1,40 @@
-const fs = require('fs')
-const path = require('path')
-const request = require('request')
-const schedule = require('node-schedule')
-const configs = require('../config.json')
+const fs = require("fs");
+const path = require("path");
+const request = require("request");
+const schedule = require("node-schedule");
+const configs = require("../config.json");
 
 const sendMsg = () => {
-    fs.readFile(path.resolve(__dirname, '../config.json'), function (err, data) {
+  fs.readFile(path.resolve(__dirname, "../config.json"), function (err, data) {
+    if (err) {
+      return console.error(err);
+    }
+    const configs = JSON.parse(data);
+    request(
+      {
+        json: true,
+        method: "POST",
+        url: configs.webhook,
+        body: configs.msg,
+      },
+      (err, res, body) => {
         if (err) {
-            return console.error(err)
+          console.error(err);
+        } else {
+          console.log("message sent", new Date().toLocaleTimeString());
+          console.log("message content", JSON.stringify(configs.msg));
         }
-        const configs = JSON.parse(data)
-        request({
-            json: true,
-            method: 'POST',
-            url: configs.webhook,
-            body: configs.msg
-        }, (err, res, body) => {
-            if (err) {
-                console.error(err)
-            } else {
-                console.log("message sent", (new Date()).toLocaleTimeString())
-                console.log("message content", JSON.stringify(configs.msg))
-            }
-        })
-
-    })
-}
+      }
+    );
+  });
+};
 
 const scheduleObjectLiteralSyntax = () => {
-    console.log("job start, current time", (new Date()).toLocaleTimeString())
-    configs.timers.forEach(timer => {
-        schedule.scheduleJob(timer, () => sendMsg())
-    });
-}
+  console.log("job start, current time", new Date().toLocaleTimeString());
+  configs.timers.forEach((timer) => {
+    schedule.scheduleJob(timer, () => sendMsg());
+  });
+};
 
-
-scheduleObjectLiteralSyntax()
-sendMsg()
+scheduleObjectLiteralSyntax();
+console.log("process start");
