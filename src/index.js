0971333f247686c494292afa1b30be24d9aea687
zsmj1994 <zsmj1994@gmail.com>
2020-04-24 11:16:29 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,40 +1,34 @@
-const fs = require("fs");
-const path = require("path");
 const request = require("request");
 const schedule = require("node-schedule");
 const configs = require("../config.json");
 
-const sendMsg = () => {
-  fs.readFile(path.resolve(__dirname, "../config.json"), function (err, data) {
-    if (err) {
-      return console.error(err);
-    }
-    const configs = JSON.parse(data);
-    request(
-      {
-        json: true,
-        method: "POST",
-        url: configs.webhook,
-        body: configs.msg,
-      },
-      (err, res, body) => {
-        if (err) {
-          console.error(err);
-        } else {
-          console.log("message sent", new Date().toLocaleTimeString());
-          console.log("message content", JSON.stringify(configs.msg));
-        }
+const sendMsg = (config) => {
+  request(
+    {
+      json: true,
+      method: "POST",
+      url: config.webhook,
+      body: config.msg,
+    },
+    (err, res, body) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log("message sent", new Date().toLocaleTimeString());
+        console.log("message content", JSON.stringify(config.msg));
       }
-    );
-  });
+    }
+  );
 };
 
 const scheduleObjectLiteralSyntax = () => {
   console.log("job start, current time", new Date().toLocaleTimeString());
-  configs.timers.forEach((timer) => {
-    schedule.scheduleJob(timer, () => sendMsg());
+  configs.forEach((config) => {
+    config.timers.forEach((timer) => {
+      schedule.scheduleJob(timer, () => sendMsg(config));
+    });
+    config.leading && sendMsg(config);
   });
 };
 
 scheduleObjectLiteralSyntax();
-console.log("process start");
